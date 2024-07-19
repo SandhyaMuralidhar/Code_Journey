6a149681b704a4e4def9e77e9810dc938e5eb74c
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-07-19 10:55:53 +0800

    Time: 119 ms (25.75%) | Memory: 16.8 MB (43.01%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
@@ -10,13 +10,13 @@ class Solution:
             min_ele= float('inf')
             for j in range(n):
                 min_ele = min(min_ele, matrix[i][j])
-            print("min_ele",min_ele)
+            
             lis1.append(min_ele)
         for j in range(n):
             max_ele = -float('inf')
             for i in range(m):
                 max_ele = max(max_ele, matrix[i][j])
-            print("max_ele",max_ele)
+            
             if max_ele in lis1:
                 out.append(max_ele)
         
