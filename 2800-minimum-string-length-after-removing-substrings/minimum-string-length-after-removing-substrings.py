f34f48fb2a85e56e1984168d20289a682a2c2f96
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-10-07 10:36:46 +0800

    Time: 54 ms (12.59%) | Memory: 16.6 MB (7.81%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
@@ -1,14 +1,30 @@
 class Solution:
     def minLength(self, s: str) -> int:
-        i=0
-        while i<len(s):
-            print(s[i:i+1])
-            if s[i:i+2] == 'AB' or s[i:i+2] == 'CD':
-                s=s[:i]+s[i+2:] 
-                i=i-1
+        # i=0
+        # while i<len(s):
+        #     print(s[i:i+1])
+        #     if s[i:i+2] == 'AB' or s[i:i+2] == 'CD':
+        #         s=s[:i]+s[i+2:] 
+        #         i=i-1
+        #     else:
+        #         i=i+1
+        # return len(s)
+
+        stack = []
+        for char in s:
+            if not stack:
+                stack.append(char)
+
+            elif char =='B' and stack[-1]=='A':
+                stack.pop()
+
+            elif char=='D' and stack[-1]=='C':
+                stack.pop()
+
             else:
-                i=i+1
-        return len(s)
+                stack.append(char)
+
+        return len(stack)
             
 
         
