2591acf97ea5b242d579d4738355e275fb0a257b
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-10-19 18:06:08 +0800

    Time: 456 ms (99.38%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,2 @@
+/* Write your PL/SQL query statement below select p.project_id, round(avg(e.experience_years),2) average_years  from project p inner join employee e on e.employee_id=p.employee_id group by p.project_id;*/
+select p.project_id, round(avg(e.experience_years),2) average_years  from project p inner join employee e on p.employee_id= e.employee_id group by  p.project_id;
