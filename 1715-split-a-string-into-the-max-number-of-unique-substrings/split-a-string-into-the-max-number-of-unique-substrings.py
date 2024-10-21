df2fce645853ba4e5d092c12109459b68e2b5f40
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-10-21 12:45:00 +0800

    Time: 206 ms (32.06%) | Memory: 16.7 MB (19.96%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.py
@@ -1,19 +1,13 @@
 class Solution:
     def maxUniqueSplit(self, s: str) -> int:
-        seen = set()
-        return self.backtrack(s, 0, seen)
-
-    def backtrack(self, s, start, seen):
-        if start == len(s):
-            return 0
-
-        max_count = 0
-
-        for end in range(start + 1, len(s) + 1):
-            sub_string = s[start:end]
-            if sub_string not in seen:
-                seen.add(sub_string)
-                max_count = max(max_count, 1 + self.backtrack(s, end, seen))
-                seen.remove(sub_string)
-
-        return max_count
+        seen=set()
+        return self.backtrack(seen, s, 0)
+    
+    def backtrack(self, seen,s,start):
+        maxcount=0
+        for end in range(start+1,len(s)+1):
+            if s[start:end] not in seen:
+                seen.add(s[start:end])
+                maxcount=max(maxcount,1+self.backtrack(seen,s,end))
+                seen.remove(s[start:end])
+        return maxcount
