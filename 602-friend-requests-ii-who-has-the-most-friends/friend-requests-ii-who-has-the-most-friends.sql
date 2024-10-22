f2b7102ff85dc44fb5d0f5a79430496524830459
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-10-22 12:27:49 +0800

    Time: 487 ms (50.30%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,7 +1,16 @@
 /* Write your PL/SQL query statement below */
+/*
 select id, num from (
 select id, num, row_number() over (order by num desc)  ra from(
 select id,sum(num) num from (
 select requester_id id, count(accepter_id) num from requestAccepted group by requester_id
 union all
 select accepter_id id, count(requester_id) num from requestAccepted group by accepter_id) group by id) ) where ra=1
+*/
+
+select requester_id id , cnt num from (
+select requester_id, rank() over (order by cnt desc) ra, cnt from(
+select requester_id, count(*) cnt from (
+select requester_id, accepter_id from requestAccepted 
+union 
+select accepter_id requester_id, requester_id accepter_id from requestAccepted) group by requester_id)) where  ra =1
