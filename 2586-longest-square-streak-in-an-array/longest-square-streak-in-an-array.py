8d2a9a0f52f8b53e1778c1c43dc144c1bacf7d5d
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-10-28 12:04:28 +0800

    Time: 111 ms (62.16%) | Memory: 33.9 MB (48.00%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
@@ -1,38 +1,16 @@
 class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
-        nums.sort()
         longest_streak=0
-        processed_nums=set()
-        for n in nums:
-            if n in processed_nums:
-                continue
-            current_length=1
+        s=set(nums)
+        for n in nums: 
+            current_streak=1
             streak=n
-            
-            while streak*streak <= 10**5:
-                if self._binary_search(nums,streak*streak):
-                    streak*=streak
-                    current_length+=1
-                    processed_nums.add(streak)
-                    
-                else:
+            while streak*streak in s:
+                if streak*streak>10**5:
                     break
-            longest_streak=max(longest_streak,current_length)
-        return longest_streak if longest_streak >=2 else -1
+                streak*=streak
+                current_streak+=1
+            longest_streak = max(longest_streak, current_streak)
+        return longest_streak if longest_streak>=2 else -1
 
 
-    def _binary_search(self, nums, n):
-        left=0
-        right=len(nums)-1
-        
-        while(left<=right):
-            mid = left+(right-left)//2
-            if nums[mid] == n:
-                return True
-            elif nums[mid]<n:
-                left = mid+1
-            elif nums[mid]>n:
-                right=mid-1
-        return False
-
-                
