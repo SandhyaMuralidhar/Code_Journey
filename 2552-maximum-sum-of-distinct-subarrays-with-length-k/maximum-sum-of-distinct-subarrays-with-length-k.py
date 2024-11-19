b55042232d47c8dc6d2857c0a92cf6d268e0cbd0
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-11-19 11:13:27 +0800

    Time: 120 ms (54.65%) | Memory: 36.8 MB (6.50%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -3,19 +3,19 @@ class Solution:
         ans=0
         start=0
         end=0
+        curr_sum=0
         num_to_index={}
-        sumnumbers=0
 
-        while end<(len(nums)):
-            number=nums[end]
-            last_occurence=num_to_index.get(number,-1)
-            
+
+        while end<len(nums):
+            curr_num = nums[end]
+            last_occurence=num_to_index.get(curr_num,-1)
             while last_occurence>=start or end-start+1>k:
-                sumnumbers-=nums[start]
+                curr_sum-=nums[start]
                 start+=1
-            num_to_index[number]=end
-            sumnumbers+=number
+            curr_sum+=curr_num
+            num_to_index[curr_num]=end
             if end-start+1==k:
-                ans=max(ans,sumnumbers)
+                ans=max(ans,curr_sum)
             end+=1
         return ans
