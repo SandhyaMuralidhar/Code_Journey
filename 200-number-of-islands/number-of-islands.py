49c468d331ad697cdbc1869a12bb168a34855d65
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-11-25 12:50:14 +0800

    Time: 224 ms (88.85%) | Memory: 18.9 MB (75.55%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,29 +1,23 @@
 class Solution:
-    def numIslands(self, grid):
+    def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-
-        num_islands = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1":
-                    self.dfs(grid, i, j)
-                    num_islands += 1
+        
+        m=len(grid)
+        n=len(grid[0])
+        num_islands=0
+        def dfs( r,c):
+            if r<0 or c<0 or r>=m or c>=n or grid[r][c]!="1":
+                return
+            grid[r][c]="0"
+            dfs(r-1,c)
+            dfs(r+1,c)
+            dfs(r,c-1)
+            dfs(r,c+1)        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]=="1":
+                    dfs(i,j)
+                    num_islands+=1
 
         return num_islands
-
-    def dfs(self, grid, r, c):
-        if (
-            r < 0
-            or c < 0
-            or r >= len(grid)
-            or c >= len(grid[0])
-            or grid[r][c] != "1"
-        ):
-            return
-        grid[r][c] = "0"
-
-        self.dfs(grid, r - 1, c)
-        self.dfs(grid, r + 1, c)
-        self.dfs(grid, r, c - 1)
-        self.dfs(grid, r, c + 1)
