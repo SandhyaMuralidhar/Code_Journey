592a96b20c5b837e6dc2cc87278f5f1806cc9efe
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-11-26 11:50:49 +0800

    Time: 6 ms (74.17%) | Memory: 17.5 MB (94.59%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.py
+++ 3189-find-champion-ii/find-champion-ii.py
@@ -1,12 +1,22 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-        indegree=[0]*n
+    
+        # Initialize the indegree array to track the number of incoming edges for each team
+        indegree = [0] * n
+
+        # Store the indegree of each team
         for edge in edges:
-            indegree[edge[1]]+=1
-        champ=-1
-        count=0
+            indegree[edge[1]] += 1
+
+        champ = -1
+        champ_count = 0
+
+        # Iterate through all teams to find those with an indegree of 0
         for i in range(n):
-            if indegree[i]==0:
-                champ=i
-                count+=1
-        return champ if count==1 else -1
+            # If the team can be a champion, store the team number and increment the count
+            if indegree[i] == 0:
+                champ_count += 1
+                champ = i
+
+        # If more than one team can be a champion, return -1, otherwise return the champion team number
+        return champ if champ_count == 1 else -1
