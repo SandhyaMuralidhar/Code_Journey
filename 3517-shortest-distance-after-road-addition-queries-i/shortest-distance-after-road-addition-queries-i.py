e72695097068f529b474eb581d11ffee34f110cb
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-11-28 14:33:20 +0800

    Time: 410 ms (82.66%) | Memory: 17.8 MB (8.40%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.py
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.py
@@ -1,39 +1,37 @@
 class Solution:
-
-    def bfs(self, adj_nodes,n):
+    def bfs(self,adj_nodes,n):
         visited=[False]*n
-        myqueue=deque()
-        myqueue.append(0)
-        visited[0]=True
-        current_layer_node_count=1
-        next_layer_node_count=0
-        layers_explored=0
-        while myqueue:
-            for _ in range(current_layer_node_count):
-                current_node = myqueue.popleft()
+        my_queue=deque()
+        my_queue.append(0)
+        current_level_total_nodes=1
+        next_level_total_nodes=0
+        total_levels=0
+        visited[0]= True
+        while my_queue:
+            for _ in range(current_level_total_nodes):
+                current_node=my_queue.popleft()
                 if current_node==n-1:
-                    return layers_explored
+                    return total_levels
+
                 for neighbor in adj_nodes[current_node]:
                     if visited[neighbor]:
                         continue
-                    myqueue.append(neighbor)
+                    my_queue.append(neighbor)
                     visited[neighbor]=True
-                    next_layer_node_count+=1
-            current_layer_node_count=next_layer_node_count
-            next_layer_node_count=0
-            layers_explored+=1
+                    next_level_total_nodes+=1
+            current_level_total_nodes=next_level_total_nodes
+            next_level_total_nodes =0
+            total_levels+=1
         return -1
-
-
-
-
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
         adj_nodes=[[] for _ in range(n)]
         answer=[]
         for i in range(n-1):
             adj_nodes[i].append(i+1)
-
+            
         for q in queries:
             adj_nodes[q[0]].append(q[1])
             answer.append(self.bfs(adj_nodes,n))
+
+
         return answer
