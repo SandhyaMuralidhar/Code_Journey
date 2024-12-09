e2f6816f757b09808ee3f736c4dc256361da75cc
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-12-09 12:54:02 +0800

    Time: 59 ms (45.83%) | Memory: 47.2 MB (30.36%) - LeetSync

--- 3427-special-array-ii/special-array-ii.py
+++ 3427-special-array-ii/special-array-ii.py
@@ -1,46 +1,39 @@
 class Solution:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
-        ans = [False] * len(queries)
-        violating_indices = []
-
-        for i in range(1, len(nums)):
-            # same parity, found violating index
-            if nums[i] % 2 == nums[i - 1] % 2:
-                violating_indices.append(i)
+        out=[False]*len(queries)
+        violating_index=[]
+        for i in range(1,len(nums)):
+            if nums[i]%2==nums[i-1]%2:
+                violating_index.append(i)
 
         for i in range(len(queries)):
-            query = queries[i]
-            start = query[0]
-            end = query[1]
-
-            found_violating_index = self.binarySearch(
-                start + 1, end, violating_indices
-            )
-
-            if found_violating_index:
-                ans[i] = False
+            q=queries[i]
+            start= q[0]
+            end=q[1]
+            val=self.binary_search(start+1,end,violating_index)
+            if val:
+                out[i]=False
             else:
-                ans[i] = True
-
-        return ans
-
-    def binarySearch(
-        self, start: int, end: int, violating_indices: List[int]
-    ) -> bool:
-        left = 0
-        right = len(violating_indices) - 1
-        while left <= right:
-            mid = left + (right - left) // 2
-            violating_index = violating_indices[mid]
-
-            if violating_index < start:
-                # check right half
-                left = mid + 1
-            elif violating_index > end:
-                # check left half
-                right = mid - 1
+                out[i]=True
+        return out
+            
+    def binary_search(self, start,end,v):
+        left=0
+        right=len(v)-1
+        
+        while left<=right:
+            mid=left+(right-left)//2
+            v_i=v[mid]
+            if v_i<start:
+                left=mid+1
+            elif v_i>end:
+                right=mid-1
             else:
-                # violatingIndex falls in between start and end
                 return True
-
         return False
+
+
+
+
+
+
