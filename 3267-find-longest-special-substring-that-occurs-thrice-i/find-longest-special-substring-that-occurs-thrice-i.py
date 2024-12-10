7dbb26cd8f93dee41fc2e45677929ee37efb0d9b
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-12-10 12:43:04 +0800

    Time: 15 ms (64.59%) | Memory: 17.4 MB (5.52%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
@@ -1,25 +1,16 @@
 class Solution:
     def maximumLength(self, s: str) -> int:
         # Create a dictionary to store the count of all substrings.
-        count = {}
-        count_strings = 0
-        for start in range(len(s)):
-            character = s[start]
-            substring_length = 0
-            for end in range(start, len(s)):
-                # If the string is empty, or the current character is equal to
-                # the previously added character, then add it to the map.
-                # Otherwise, break the iteration.
-                if character == s[end]:
-                    substring_length += 1
-                    count[(character, substring_length)] = (
-                        count.get((character, substring_length), 0) + 1
-                    )
+        count={}
+        for i in range (len(s)):
+            character_count=0
+            character = s[i]
+            for j in range(i,len(s)):
+                if character==s[j]:
+                    character_count+=1
+                    count[(character,character_count)]=count.get((character,character_count),0)+1
                 else:
                     break
-
-        # Create a variable ans to store the longest length of substring with
-        # frequency atleast 3.
         ans = 0
         for i in count.items():
             length = i[0][1]
