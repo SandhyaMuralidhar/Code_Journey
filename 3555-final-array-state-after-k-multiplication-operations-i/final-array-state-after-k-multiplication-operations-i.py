b5fafed87f184e9160711587a12f158f7d47fdf6
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-12-16 15:49:20 +0800

    Time: 3 ms (52.21%) | Memory: 17.2 MB (28.27%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
@@ -9,10 +9,23 @@ class Solution:
             
         #     k-=1
         # return nums
-        for _ in range(k):
-            min_element=0
-            for i in range(len(nums)):
-                if nums[i] < nums[min_element]:
-                    min_element=i
-            nums[min_element]*=multiplier
-        return nums
+        # for _ in range(k):
+        #     min_element=0
+        #     for i in range(len(nums)):
+        #         if nums[i] < nums[min_element]:
+        #             min_element=i
+        #     nums[min_element]*=multiplier
+        # return nums
+
+        heap=[]
+        for i, num in enumerate(nums):
+            heappush(heap,(num,i))
+        for i in range(k):
+            n,i=heappop(heap)
+            new_val = n*multiplier
+            heappush(heap,(new_val,i))
+        heap.sort(key =lambda k: k[1])
+        res=[]
+        for element in heap:
+            res.append(element[0])
+        return res
