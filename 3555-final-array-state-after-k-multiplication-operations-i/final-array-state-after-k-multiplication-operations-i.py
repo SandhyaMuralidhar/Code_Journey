f771803fee51d89304a6f749615035f6ce856546
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2024-12-16 14:35:38 +0800

    Time: 3 ms (52.21%) | Memory: 17.5 MB (11.77%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
@@ -1,11 +1,18 @@
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
-        while k>0:
+        # while k>0:
             
-            min_ele=min(nums)
-            ind=nums.index(min_ele)
-            nums[ind]=nums[ind]*multiplier
+        #     min_ele=min(nums)
+        #     ind=nums.index(min_ele)
+        #     nums[ind]=nums[ind]*multiplier
             
             
-            k-=1
+        #     k-=1
+        # return nums
+        for _ in range(k):
+            min_element=0
+            for i in range(len(nums)):
+                if nums[i] < nums[min_element]:
+                    min_element=i
+            nums[min_element]*=multiplier
         return nums
