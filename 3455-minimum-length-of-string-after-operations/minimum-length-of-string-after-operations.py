6af02191c21adf07e2968c1f8b96e1a861f316fa
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2025-01-13 11:20:58 +0800

    Time: 5519 ms (5.08%) | Memory: 21.7 MB (5.71%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -0,0 +1,41 @@
+class Solution:
+    def find_left(self,ch,i,c):
+        for j in range(i-1,-1,-1):
+            if ch==c[j]:
+                return j
+        return -1
+
+    def find_right(self,ch,i,c):
+        for j in range(i+1,len(c)):
+            if ch==c[j]:
+                return j
+        return -1
+
+
+
+    def minimumLength(self, s: str) -> int:
+        c=list(s)
+        i=1
+        while i<(len(s)):
+            ch=c[i]
+            if ch!='':
+                l=self.find_left(ch,i,c)
+                r=self.find_right(ch,i,c)
+                #print(ch,l,r,i)
+                if l!=-1 and r!=-1:
+                    # c.pop(r)
+                    # c.pop(l)
+                    
+                    # c.insert(l,'')
+                    # c.insert(r,'')
+                    c[l]=''
+                    c[r]=''
+                  
+            i+=1
+        count=0
+        for i in range(len(c)):
+            if c[i]!='':
+                count+=1
+        return count
+
+    
