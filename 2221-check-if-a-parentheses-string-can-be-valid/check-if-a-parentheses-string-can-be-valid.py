27085c0942f62079d3dbfe73246d34f0fe2eedce
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2025-01-17 13:06:13 +0800

    Time: 65 ms (72.46%) | Memory: 22 MB (11.67%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
@@ -1,34 +1,30 @@
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
-        length = len(s)
-
-        # If length of string is odd, return false.
-        if length % 2 == 1:
+        l =len(s)
+        if l%2==1:
             return False
-
-        open_brackets = []
-        unlocked = []
-
-        # Iterate through the string to handle '(' and ')'
-        for i in range(length):
-            if locked[i] == "0":
+        unlocked=[]
+        open_brackets=[]
+        for i in range(l):
+            if locked[i]=="0":
                 unlocked.append(i)
-            elif s[i] == "(":
+            elif s[i]=="(":
                 open_brackets.append(i)
-            elif s[i] == ")":
+            elif s[i]==")":
                 if open_brackets:
                     open_brackets.pop()
                 elif unlocked:
                     unlocked.pop()
                 else:
                     return False
-
-        # Match remaining open brackets and the unlocked characters
-        while open_brackets and unlocked and open_brackets[-1] < unlocked[-1]:
+            
+        
+        while open_brackets and unlocked and open_brackets[-1]<unlocked[-1]:
             open_brackets.pop()
             unlocked.pop()
-
         if open_brackets:
             return False
-
         return True
+                    
+
+
