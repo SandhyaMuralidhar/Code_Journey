7901b9d7a4b4ca2466deba7e2466a14b70203010
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2025-01-23 14:27:16 +0800

    Time: 15 ms (73.78%) | Memory: 19.8 MB (17.23%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.py
@@ -1,18 +1,17 @@
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
-        a=set()
-        row=[0]*len(grid[0])
-        col=[0]*len(grid)
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]==1:
-                    row[j]+=1
-                    col[i]+=1
-        communicable_servers_count=0
+        row={}
+        col={}
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j]:
-                    if row[j] > 1 or col[i] > 1:
-                        communicable_servers_count += 1
+                    row[i]=row.get(i,0)+1
+                    col[j]=col.get(j,0)+1
+        out=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]:
+                    if row[i]>1 or col[j]>1:
+                        out+=1
 
-        return communicable_servers_count
+        return out
