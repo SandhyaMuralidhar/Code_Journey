e22c2a818a3fda677cdc00829628f703670b6f7f
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2025-02-10 13:01:04 +0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (70.70%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -1,21 +1,29 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        s_array=list(s)
+        # s_array=list(s)
+        # i=0
+        # while i<len(s_array):
+        #     if s_array[i].isnumeric():
+        #         j=i-1
+        #         while(j>=0):
+        #             if s_array[j]!="Marked" and s_array[j].isalpha():
+        #                 s_array[j]="Marked"
+        #                 break
+        #             j-=1
+        #     i+=1
+        #     #print(s_array)
+        # out=''
+        # i=0
+        # while i<len(s):
+        #     if s_array[i]!="Marked" and not s_array[i].isnumeric():  
+        #         out+=s[i] 
+        #     i+=1
+        # return out
+        l=[]
         i=0
-        while i<len(s_array):
-            if s_array[i].isnumeric():
-                j=i-1
-                while(j>=0):
-                    if s_array[j]!="Marked" and s_array[j].isalpha():
-                        s_array[j]="Marked"
-                        break
-                    j-=1
-            i+=1
-            #print(s_array)
-        out=''
-        i=0
-        while i<len(s):
-            if s_array[i]!="Marked" and not s_array[i].isnumeric():  
-                out+=s[i] 
-            i+=1
-        return out
+        for i in range(len(s)):
+            if s[i].isalpha():
+                l.append(s[i])
+            else:
+                l.pop()
+        return ''.join(l)
