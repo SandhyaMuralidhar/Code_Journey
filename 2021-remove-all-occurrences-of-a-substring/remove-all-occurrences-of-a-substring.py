bcbdb959b56e2e1b4f109628a9c72332ee3b9a5b
SandhyaMuralidhar <152950539+SandhyaMuralidhar@users.noreply.github.com>
2025-02-11 10:52:14 +0800

    Time: 7 ms (38.80%) | Memory: 17.8 MB (75.36%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
@@ -3,12 +3,9 @@ class Solution:
         while part in s:
             i=0
             while i<len(s):
-                #print(s[i:i+len(part)])
                 if s[i:i+len(part)]==part:
                     s=s[:i]+s[i+len(part):]
-                    #print("Matched",s)
                     i=0
                 else:
                     i+=1
-        #print(s)
         return s
